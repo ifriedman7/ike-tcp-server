4f70281cf335819d87b7fa8910ceb975561c550a
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-12-07 16:17:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ serversocket.bind((host, port))
 serversocket.listen(3)
 while True:
     clientsocket, address = serversocket.accept()
-    print("received connection from " % str(address))
+    print("received connection from %s" % str(address))
     message = "Thank you for connecting.\r\n"
     clientsocket.send(message.encode('ascii'))
     clientsocket.close()
