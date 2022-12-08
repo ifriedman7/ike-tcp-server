6a0cb05373cf5b5f10b3d5c8f990e6837ac3935c
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-12-08 12:13:05 +0200

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import nmap, socket
+
+scanner = nmap.PortScanner()
+
+print("Welcome. This is nmap automation tool.")
+
+ip_addr = input("Enter IP address to scan (default localhost): ")
+if ip_addr == '':
+    ip_addr = socket.gethostname()
+print("IP entered: ", ip_addr)
+type(ip_addr)
+
+resp = input(""" \nEnter scan type:
+                1) SYN ACK scan
+                2) UDP scan
+                3) Comprehensive scan""")
+print("Option selected: ", resp)
+
+if resp == '1':
+    print("Nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-3000', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP state: ", scanner[ip_addr].state)
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports: ", scanner[ip_addr]['tcp'].keys())
